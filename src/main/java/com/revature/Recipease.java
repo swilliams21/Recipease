59fdb5de177de1857579228dd0d0301a04627342
Abatson <abatson94@gmail.com>
2019-09-04 09:56:31 -0400

    renamed app

--- src/main/java/com/revature/Recipease.java
+++ src/main/java/com/revature/Recipease.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GoFundYouApplication {
+public class Recipease {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GoFundYouApplication.class, args);
+		SpringApplication.run(Recipease.class, args);
 	}
 
 }
